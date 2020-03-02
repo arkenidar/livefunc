62569f53c7ed4c3255a605141c6905a7f0394da3
Dario Cangialosi <dario.cangialosi@gmail.com>
2020-03-02 21:36:11 +0100

    'console.log' callable switching an if parameter was the beginning of better understanding of code that led to these various changes

--- myfunc-current.js
+++ myfunc-current.js
@@ -3,21 +3,30 @@
 function def_func(function_def){
     var defined=function defined(...farguments){
         var fcontext={}
-        for(argidx in function_def.arguments) fcontext[function_def.arguments[argidx]]=farguments[argidx]
+        for(var argidx in function_def.arguments) fcontext[function_def.arguments[argidx]]=farguments[argidx]
         fcontext[function_def.name]=defined
         return callfunc(function_def, fcontext)
     }
-    this[function_def.name]=defined
+    // test the return with assignment even of anonymous functions
+    return this[function_def.name]=defined
 }
 
 function callfunc(function_def, fcontext){
     function handle_argument(argument){
+        if(typeof argument=='string') return eval_statement(fcontext,argument)
+        
         // bad for use of eval but quick to implement
         ///var func=eval(argument[0])
         var func // function or not
-        if(argument[0] in this && (func=this[argument[0]])
-        && typeof func=='function'){
+        if(true) // switch this for comparison
+        func=eval_statement(fcontext,argument[0])
+        else
+        // this does not yet recognize expressions e.g. "console.log"
+        if(argument[0] in this) func=this[argument[0]]
+
+        if(typeof func=='function'){
             var args=argument.slice(1)
+            args=args.map(exec_statement)
             return func(...args)
         }
 
@@ -25,13 +34,15 @@ function callfunc(function_def, fcontext){
         if(Array.isArray(argument) && argument.length>0 &&
         (specials.indexOf(argument[0])!=-1 || argument[0] in this)
         ) return exec_statement(argument)
-        if(typeof argument=='string') return exec_statement(argument)
+
         return argument
     }
 
+    var value
     for(statement of function_def.statements)
-        var value=exec_statement(statement)
-    
+        value=exec_statement(statement)
+    return value
+
     function exec_statement(statement){
         var value
         if(Array.isArray(statement)){
@@ -40,44 +51,47 @@ function callfunc(function_def, fcontext){
                 while(true){
                     if(!(idx in statement)) break
                     if(idx+1 in statement){
-                        if(true==handle_argument(statement[idx])){
-                            value=handle_argument(statement[idx+1])
+                        if(true==exec_statement(statement[idx])){
+                            value=exec_statement(statement[idx+1])
                             break
                         }
                     }else
-                    value=handle_argument(statement[idx])
+                    value=exec_statement(statement[idx])
                     idx+=2
                 }
             }else if(statement[0]=='write'){
-                var line=handle_argument(statement[1])
+                var line=exec_statement(statement[1])
                 console.log(line)
             }else if(statement[0]=='block'){
                 for(var substatement of statement.slice(1)) value=exec_statement(substatement)
             }else{
-                value='error: unknown statement: '+statement[0]
+                // generic or unrecognized statement
+                // statement as argument
+                // todo: verify if proper
+                value=handle_argument(statement)
             }
         }else if(typeof statement=='string'){
             value=eval_statement(fcontext,statement)
+        }else{
+            value=statement
         }
         
         return value
-
-        function eval_statement(fcontext,statement){
-            var value = (function(fcontext,statement){
-                for(variable in fcontext)
-                  eval(variable+"="+fcontext[variable])
-                return eval(statement)
-              })(fcontext,statement)
-            return value
-        }
     }
-    return value
+    function eval_statement(fcontext,statement){
+        var value = (function(fcontext,statement){
+            for(variable in fcontext)
+              eval(variable+"="+fcontext[variable])
+            return eval(statement)
+          })(fcontext,statement)
+        return value
+    }
 }
 ////////////// sum
 var def_mysum={"name":"mysum","arguments":["x","y"],
 "statements":["x+y"]
 }
-def_func(def_mysum)
+def_func(def_mysum) // it can be defined with ['def_func',def_mysum], also
 console.log(mysum(14,3))
 ////////////// pow
 var statements1=[`if(exponent<0) returned=1/mypow(base,-exponent)
@@ -93,6 +107,7 @@ var statements3=[['if','exponent<0','1/mypow(base,-exponent)',
 
 var statements4=[
     ['write','"abc"'], // passing a string
+    ['console.log','"abc from console.log()"'], // todo: this should work too!
     ['write','variable_test'], // passing a variable
     ['write',[1,2,3]], // passing a list
     ['if','true',['write','"if reached"']],
